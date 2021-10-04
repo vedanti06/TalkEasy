802100662d2f3f876f35f51f91fb278366bf60df
Vidhii19 <ruparelvd0919@gmail.com>
2021-10-04 16:40:39 +0530

    changed dashboard ui

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:talkeasy_loginsignup/departments.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -35,51 +36,60 @@ class Dashboard extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
-            iconTheme: IconThemeData(color: Colors.black),
-            title: Text("TalkEasy",style: TextStyle(color: Colors.black)),
-            backgroundColor: Colors.lightGreenAccent,
+            iconTheme: IconThemeData(color: Colors.white),
+            title: Text("Talk Easy",style: GoogleFonts.openSans(
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,)),
+            ),
+            backgroundColor: Colors.teal,
           ),
           drawer: Drawer(
-            child: ListView(padding: EdgeInsets.all(8.0), children: [
-              UserAccountsDrawerHeader(
-                accountName: Text("Name"),
+            child: Container(
+                color: Colors.grey[300],
+              child: ListView(padding: EdgeInsets.all(8.0), children: [
+                const DrawerHeader(
+                    decoration: BoxDecoration(color: Colors.white24,), child: null,
+                  // child: Text('Name'),
               ),
-              ListTile(
-                leading: Icon(Icons.home,color: Colors.deepPurple,),
-                title: Text("Home",style: TextStyle(fontWeight: FontWeight.w700,),),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Dashboard()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.person_search_sharp,color: Colors.deepPurple,),
-                title: Text("Departments",style: TextStyle(fontWeight: FontWeight.w700,),),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Departments()),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.gps_fixed,color: Colors.deepPurple,),
-                title: Text("Maps",style: TextStyle(fontWeight: FontWeight.w700,),),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.rate_review,color: Colors.deepPurple,),
-                title: Text("Review",style: TextStyle(fontWeight: FontWeight.w700,),),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.logout,color: Colors.deepPurple,),
-                title: Text("Logout",style: TextStyle(fontWeight: FontWeight.w700,),),
-                onTap: () {},
-              ),
-            ]),
+                ListTile(
+                  leading: Icon(Icons.home,color: Colors.black,),
+                  title: Text("Home",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Dashboard()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.person_search_sharp,color: Colors.black,),
+                  title: Text("Departments",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Departments()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.gps_fixed,color: Colors.black,),
+                  title: Text("Maps",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.rate_review,color: Colors.black,),
+                  title: Text("Review",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout,color: Colors.black,),
+                  title: Text("Logout",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+                  onTap: () {},
+                ),
+              ]),
+            ),
           ),
           body:(
               Column(
--- lib/departments.dart
+++ lib/departments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:talkeasy_loginsignup/dashboard.dart';
 import 'dept.dart';
 void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false,home: Departments()));
 
@@ -12,6 +13,62 @@ class DepartmentsState extends State<Departments> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text("Departments",style: GoogleFonts.openSans(
+            textStyle: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,)),
+        ),
+        backgroundColor: Colors.teal,
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.grey[300],
+          child: ListView(padding: EdgeInsets.all(8.0), children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.white24,), child: null,
+              // child: Text('Name'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,color: Colors.black,),
+              title: Text("Home",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Dashboard()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person_search_sharp,color: Colors.black,),
+              title: Text("Departments",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Departments()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.gps_fixed,color: Colors.black,),
+              title: Text("Maps",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.rate_review,color: Colors.black,),
+              title: Text("Review",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.logout,color: Colors.black,),
+              title: Text("Logout",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.w600,),),
+              onTap: () {},
+            ),
+          ]),
+        ),
+      ),
       backgroundColor: Colors.white,
       body: Column(
         children: <Widget>[
@@ -26,14 +83,14 @@ class DepartmentsState extends State<Departments> {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                      "Departments",
-                      style: GoogleFonts.openSans(
-                          textStyle: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold)),
-                    ),
+                    // Text(
+                    //   "Departments",
+                    //   style: GoogleFonts.openSans(
+                    //       textStyle: TextStyle(
+                    //           color: Colors.black,
+                    //           fontSize: 18,
+                    //           fontWeight: FontWeight.bold)),
+                    // ),
                     SizedBox(
                       height: 2,
                     ),
